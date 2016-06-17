58ffc5ae494ff80bc850e1ba4d405981b8a98434
w <w@w.com>
2016-06-17 10:19:14 +0800

    main.js require fastclick

--- app/main/main.js
+++ app/main/main.js
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-// require('react-fastclick');
+require('react-fastclick');
 
 // var EventBus = require('eventbusjs');
 var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
